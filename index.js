8b91a5c96d0620f57fd23862a90e67a685d57742
Aries Beltran <aries.beltran@gmail.com>
2014-07-09 10:20:13 +0800

    Initial commit. Using Jasmine 1.3.x

---
+++ index.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var fs = require('fs');
+var simplet = require('simplet');
+
+
+var JasmineRunnerReporter = function(config, logger, helper) {
+  var log = logger.create('reporter.jasmine-runner');
+  var reporterConfig = config.jasmineRunnerReporter || {};
+  var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, 
+      reporterConfig.outputFile || 'jasmine-runner.html'));
+  var tmpl = '<script type="text/javascript" src="$file"></script>';
+  var runnerBase = path.dirname(outputFile);
+ 
+
+  this.adapters = [];
+
+  this.onRunStart = function(browsers) {
+    var files = config.files;
+    var specs = [];
+    files.forEach(function(file) {
+      if( file.watched ) {
+        var sourceFile = helper.normalizeWinPath(path.relative(runnerBase, file.pattern));
+        specs.push(tmpl.replace('$file', sourceFile));
+      }
+    });
+    
+    var fileEngine = simplet();
+    var output = fileEngine.render( __dirname + '/runner-template.html', {
+        lib: helper.normalizeWinPath(path.relative(runnerBase, __dirname)),
+        specs: specs.join('\n')
+    });
+    
+    helper.mkdirIfNotExists(path.dirname(outputFile), function() {
+      fs.writeFile(outputFile, output, function(err) {
+        if (err) {
+          log.warn('Cannot write Jasmine Runner HTML\n\t' + err.message);
+        } else {
+          log.debug('Jasmine Runner results written to "%s".', outputFile);
+        }
+      });
+    });
+  };
+
+};
+
+JasmineRunnerReporter.$inject = ['config', 'logger', 'helper'];
+
+module.exports = {
+  'reporter:jasmine-runner': ['type', JasmineRunnerReporter]
+};
